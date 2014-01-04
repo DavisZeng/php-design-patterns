35ecb50c374556e75409e67bc07fc3235df5c165
Dewu Zeng <dewuzeng@gmail.com>
2014-01-04 17:19:48 +0800

    add Simple Factory Pattern

--- Factory/SimpleFactory/Client.php
+++ Factory/SimpleFactory/Client.php
@@ -28,14 +28,14 @@ class Client
         echo "29 + 2013 = " . $operator->getResult() . "\n";
 
         $operator = OperationFactory::createOperation(OperationFactory::OPERATION_SUB);
-        $operator->numberA = 29;
-        $operator->numberB = 2013;
-        echo "29 - 2013 = " . $operator->getResult() . "\n";
+        $operator->numberA = 300;
+        $operator->numberB = 4499;
+        echo "300 - 4499 = " . $operator->getResult() . "\n";
 
         $operator = OperationFactory::createOperation(OperationFactory::OPERATION_MUL);
-        $operator->numberA = 29;
-        $operator->numberB = 2013;
-        echo "29 * 2013 = " . $operator->getResult() . "\n";
+        $operator->numberA = 13;
+        $operator->numberB = 14;
+        echo "13 * 14 = " . $operator->getResult() . "\n";
     }
 }
 Client::main();
