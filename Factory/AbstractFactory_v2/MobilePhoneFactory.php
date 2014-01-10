91b6c857b6ea50123fe1d9eb433b72f69b18b3a4
Dewu Zeng <dewuzeng@gmail.com>
2014-01-10 10:14:29 +0800

    udpate Factory Pattern

--- Factory/AbstractFactory_v2/MobilePhoneFactory.php
+++ Factory/AbstractFactory_v2/MobilePhoneFactory.php
@@ -1,10 +1,11 @@
 <?php
- /**
- * Created by PhpStorm.
+/**
+ * 抽象工厂模式，代码模板
+ * 模式意图 ：提供一个创建一系列想着或相互依赖对象的接口，而无需指定它们具体的类。
+ *
  * Date: 1/4/14
- * Time: 17:45
- * Author: zengdewu <dewuzeng@amwares.com>
- * Copyright: (C) 2013, AMware System Technologies Ltd., Co.
+ * Time: 17:09
+ * Author: zengdewu <dewuzeng@gmail.com>
  */
 
 class MobilePhoneFactory 
--- Factory/AbstractFactory_v2/Reader.php
+++ Factory/AbstractFactory_v2/Reader.php
@@ -1,12 +1,12 @@
 <?php
- /**
- * Created by PhpStorm.
+/**
+ * 抽象工厂模式，代码模板
+ * 模式意图 ：提供一个创建一系列想着或相互依赖对象的接口，而无需指定它们具体的类。
+ *
  * Date: 1/4/14
- * Time: 18:39
- * Author: zengdewu <dewuzeng@amwares.com>
- * Copyright: (C) 2013, AMware System Technologies Ltd., Co.
+ * Time: 17:09
+ * Author: zengdewu <dewuzeng@gmail.com>
  */
-
 class Reader 
 {
     const CONFIG_FILE_PATH = 'config.json';
